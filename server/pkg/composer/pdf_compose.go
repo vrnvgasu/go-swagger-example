4cdb2830870508f79be0feaa79c8d54dc8e41eb0
nnaumchenko <dhnikolas@gmail.com>
2020-11-03 03:15:28 +0300

    Compose

--- server/pkg/composer/pdf_compose.go
+++ server/pkg/composer/pdf_compose.go
@@ -2,12 +2,15 @@ package composer
 
 import (
 	"bytes"
+	"fmt"
 	"image"
 	"io"
 	"io/ioutil"
+	"log"
+	"os"
+	"path/filepath"
 	"strconv"
 
-	"github.com/go-openapi/runtime"
 	"github.com/jung-kurt/gofpdf"
 )
 
@@ -15,41 +18,50 @@ func ComposeFromFiles(files []io.ReadCloser) (io.ReadCloser, error) {
 	var opt gofpdf.ImageOptions
 	pdf := gofpdf.New("P", "mm", "A4", "")
 
+	dir, err := ioutil.TempDir("", "pdfcomp")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer os.RemoveAll(dir)
+
 	for i, f := range files {
-		if f == nil {
+
+		tmpfn := filepath.Join(dir, fmt.Sprintf("tmpfile_%d", i))
+		data, _ := ioutil.ReadAll(f)
+		if err := ioutil.WriteFile(tmpfn, data, 0666); err != nil {
+			log.Fatal(err)
+		}
+		tmpf, _ := os.Open(tmpfn)
+
+		if tmpf == nil {
 			continue
 		}
-		m, format, err := image.DecodeConfig(f)
+		m, format, err := image.DecodeConfig(tmpf)
 		if err != nil {
 			continue
 		}
-		fR, ok := f.(*runtime.File)
-		if !ok {
-			continue
-		}
-		_, err = fR.Data.Seek(0, 0)
-		if err != nil {
-			continue
-		}
+		tmpf.Seek(0, 0)
 		weigh, height := calculateRotation(m.Width, m.Height)
 		if weigh > 0 {
 			pdf.AddPage()
 		}
-		_ = pdf.RegisterImageReader("file"+strconv.Itoa(i), format, f)
+		_ = pdf.RegisterImageReader("file"+strconv.Itoa(i), format, tmpf)
 		pdf.ImageOptions("file"+strconv.Itoa(i), 0, 0, weigh, height, true, opt, 0, "")
-		f.Close()
+		tmpf.Close()
 	}
 
 	fb := []byte{}
 	rw := bytes.NewBuffer(fb)
 
-	err := pdf.Output(rw)
+	err = pdf.Output(rw)
 	if err != nil {
 		return nil, err
 	}
 
-	return ioutil.NopCloser(rw), nil
+	o := ioutil.NopCloser(rw)
+	return o, nil
 }
+
 
 func calculateRotation(w, h int) (width, height float64) {
 	x := float32(w)
